8e990e497927e3554699f8ebb005829b170d9bc3
ajaynagotha <53925991+ajaynagotha@users.noreply.github.com>
2025-08-07 02:22:57 +0530

    fix: add missing express entrypoint to tsup config (#96)
    # Description
    
    - Add Missing Express entrypoint to tsup config to fix import issues
    
    Fixes #94  🦕
    
    Co-authored-by: swapydapy <agarwal.swapy@gmail.com>

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts", "src/server/index.ts", "src/client/index.ts"],
+  entry: ["src/index.ts", "src/server/index.ts", "src/server/express/index.ts", "src/client/index.ts"],
   format: ["esm", "cjs"],
   dts: true,
 });
